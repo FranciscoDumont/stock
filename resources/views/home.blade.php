0bd4c2a30ceab872fe11a931938e234f93d7e6f8
Fran <franciscodumont1@gmail.com>
2021-02-16 16:28:20 -0300

    :building_construction: Cambios iniciales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://cdn.shopify.com/s/files/1/0103/8482/products/00_Unicorn_Giant_Meowchi.jpg?v=1506808766" height="200px;" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Hola</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>100</strong> products</div>
+                <div class="pr-5"><strong>50</strong> near expiration</div>
+                <div class="pr-5"><strong>24</strong> expired</div>
             </div>
+            <div class="pt-4 font-weight-bold">username</div>
+            <div>descripcion</div>
+            <div class="font-weight-bold"><a href="#">un-link.com</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://i.pinimg.com/originals/be/d4/6c/bed46c1177df1ccb545d66ccdb62777e.jpg" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://i.pinimg.com/originals/be/d4/6c/bed46c1177df1ccb545d66ccdb62777e.jpg" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://i.pinimg.com/originals/be/d4/6c/bed46c1177df1ccb545d66ccdb62777e.jpg" class="w-100" alt="">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                <div><img src="images/kitty.png" style="max-height: 40px; border-right: 1px solid #aaaaaa" class="pr-3"></div>
+                <div class="pl-3">{{ config('app.name', 'Laravel') }}</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -45,7 +46,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -54,7 +55,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">

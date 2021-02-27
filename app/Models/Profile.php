0b0fc5ee2fd42930db1ff1453be1982269375407
Fran <franciscodumont1@gmail.com>
2021-02-27 00:40:20 -0300

    :spaghetti: ide helper

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Profile
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $description
+ * @property string|null $url
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Profile extends Model
 {
     use HasFactory;

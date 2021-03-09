2f103ddd593a9c65c601faaa3e05bd7bd3d0eff8
Fran <franciscodumont1@gmail.com>
2021-03-08 23:56:56 -0300

    :house: Home view

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/profile/' . Auth::user()->username);
-//    return view('welcome');
-})->middleware('auth');
+Route::get('/', [PostController::class, 'index'])->middleware('auth');
 
 Auth::routes();
 

ac101bce6dfbea15fd09ddb60170ffa53f52c434
Fran <franciscodumont1@gmail.com>
2021-03-29 16:53:46 -0300

    :octopus: Livewire render

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\Stock;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // User
+        $user = new User;
+        $user->name = 'Teke';
+//        $user->username = 'teketeke';
+        $user->email = 'teke@gmail.com';
+        $user->password = bcrypt('1234567890');
+        $user->save();
+
+        // Product
+        $product = new Product();
+        $product->name = 'Hola';
+        $product->save();
+
+        // Stock
+        $stock = new Stock();
+        $stock->stock = 2;
+        $stock->user()->associate($user);
+        $stock->product()->associate($product);
+        $stock->save();
+
+        // Todo
+        // Hacer unos seeders piolas con factory
+
     }
 }

7c9df5e3907054c33e69f52d6249d763b2d5b0a8
Fran <franciscodumont1@gmail.com>
2021-04-02 14:55:46 -0300

    :information_source: Ide Helper

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -5,6 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Stock
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property int $user_id
+ * @property int $stock
+ * @property mixed|null $expiration
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Product $product
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\StockFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereExpiration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Stock extends Model
 {
     use HasFactory;

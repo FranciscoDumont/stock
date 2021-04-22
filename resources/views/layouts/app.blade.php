21151f173bf49c6963b2100f7d5a8b405132bdf2
Fran <franciscodumont1@gmail.com>
2021-04-22 20:50:04 -0300

    :rocket: TurboLinks fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,9 +47,7 @@
         @stack('modals')
 
         @livewireScripts
-        <script src="https://cdn.jsdelivr.net/npm/turbolinks@5.2.0/dist/turbolinks.min.js"></script>
         <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
-
         <script>
             Livewire.on('alert', event => {
                 Swal.fire({

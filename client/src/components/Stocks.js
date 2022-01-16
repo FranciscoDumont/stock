6fdc817240161390dd3976f73e8220e6e6fc9cd5
Fran <franciscodumont1@gmail.com>
2022-01-16 13:13:43 -0300

    :plate_with_cutlery: Consuming API

--- client/src/components/Stocks.js
+++ client/src/components/Stocks.js
@@ -1,24 +1,43 @@
 import './Stocks.css'
 import StockItem from './StockItem';
 import { useEffect, useState } from 'react';
-import stockdb from '../stockdb.json'
 
 
 function Stocks(props) {
-  const [stocks, setStocks] = useState(stockdb);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
-    console.log(stocks)
+    fetch('http://127.0.0.1:8000/api/stocks')
+    .then(response => response.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setStocks(result)
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
   }, []);
 
 
-  return (
-    <div className="expenses">
-      {stocks.map(stock => {
-        return <StockItem stock={stock}/>
-      })}
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="expenses">
+        {stocks.map(stock => {
+          console.log(stock);
+          return <StockItem key={stock.id} stock={stock}/>
+        })}
+      </div>
+    );
+  }
 }
 
 export default Stocks;
